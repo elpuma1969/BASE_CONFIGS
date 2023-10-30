4a18f4a74d113efa51f78ad8b894c0758782f209
Puma_1969 <67933513+elpuma1969@users.noreply.github.com>
2023-10-29 20:06:46 -0400

    DELL_complete_SEND.py

--- DELL_complete_SEND.py
+++ DELL_complete_SEND.py
@@ -12,7 +12,7 @@ with open("devices.txt", "r") as devices_file:
     device_ips = devices_file.read().splitlines()
 
 # List of show commands to send for Dell OS6 switches
-show_commands = ["show version", "show interfaces"]
+show_commands = ["show version", "show interfaces status", "show vlan-switch", "show running-config"]
 
 # Get the current date and time
 current_datetime = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
@@ -21,28 +21,34 @@ current_datetime = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
 for device_ip in device_ips:
     # Define the device connection parameters for each device (Dell OS6)
     dell_device = {
-        "device_type": "dell_os6",
+        "device_type": "cisco_ios",
         "ip": device_ip,
-        "username": "xxxxxx",
-        "password": "xxxxx",
+        "username": "puma",
+        "password": "cisco",
         "port": 22,
     }
 
-    # Create an SSH connection to the Dell switch
-    conn = ConnectHandler(**dell_device)
+    try:
+        # Create an SSH connection to the Dell switch
+        conn = ConnectHandler(**dell_device)
 
-    # Retrieve the hostname of the device
-    hostname = conn.find_prompt().strip("> ")
+        # Retrieve the hostname of the device
+        prompt = conn.find_prompt()
+        hostname = prompt[0:-1]
 
-    # Send commands to the device and save the output to a file
-    for cmd in show_commands:
-        output = conn.send_command(cmd)
-        # Create a filename based on the hostname, device IP, command, and date
-        file_name = f"{hostname}_{device_ip}_{cmd.replace(' ', '_')}_{current_datetime}.txt"
-        file_path = os.path.join(output_folder, file_name)
-        with open(file_path, "w") as output_file:
-            output_file.write(output)
-        print(f"Device IP: {device_ip}, Hostname: {hostname}, Command: {cmd}\nOutput saved to {file_path}\n")
+        # Send commands to the device and save the output to a file
+        for cmd in show_commands:
+            output = conn.send_command(cmd)
+            # Create a filename based on the hostname, device IP, command, and date
+            file_name = f"{hostname}_{device_ip}_{cmd.replace(' ', '_')}_{current_datetime}.txt"
+            file_path = os.path.join(output_folder, file_name)
+            with open(file_path, "w") as output_file:
+                output_file.write(output)
+            print(f"Device IP: {device_ip}, Hostname: {hostname}, Command: {cmd}\nOutput saved to {file_path}\n")
 
-    # Disconnect from the Dell switch
-    conn.disconnect()
+        # Disconnect from the Dell switch
+        conn.disconnect()
+
+    except Exception as e:
+        # Handle exceptions here, you can print an error message or log the exception
+        print(f"Error connecting to device {device_ip}: {str(e)}")
